08fdca42118a3dee4d0d04f8c6a9b6ddc45926bb
Abhithasal12 <githubabhi@gmail.com>
2025-02-12 14:55:13 +0530

    updated

--- main1.py
+++ main1.py
@@ -4,12 +4,14 @@ import tkinter as tk
 
 calculation = "" 
 
-
 def add_to_calculation(symbol):
     global calculation
-    calculation += str(symbol)
-    text_result.delete(1.0, "end")
-    text_result.insert(1.0, calculation)
+    if symbol =="%":
+        calculation += "/100"
+    else:
+        calculation += str(symbol)
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
 
 def evaluate_calculation():
     global calculation
@@ -24,13 +26,21 @@ def evaluate_calculation():
         text_result.insert(1.0, "Error")
 
 
+
 def clear_filed():
     calculation = ""
     text_result.delete(1.0, "end")
 
-# using tkinter as tk   
+def delete_filed():
+    global calculation
+    calculation = calculation[:-1]
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)    
+
 root = tk.Tk()
-root.geometry("400x275")
+root.geometry("300x325")
+root.title("calculator")
+
 
 text_result = tk.Text(root, height=2, width=16, font=("Arial",24),background="Gray")
 text_result.grid(columnspan=5)
@@ -63,7 +73,7 @@ btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5
 btn_9.grid(row=4, column=3)
 
 btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, font= ("arial",14),background="yellow")
-btn_0.grid(row=5, column=2)# 
+btn_0.grid(row=5, column=2)
 
 btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, font= ("arial",14),background="yellow")
 btn_plus.grid(row=2, column=4)
@@ -77,16 +87,22 @@ btn_mul.grid(row=4, column=4)
 btn_div = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5, font= ("arial",14),background="yellow")
 btn_div.grid(row=5, column=4)
 
-btn_open = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font= ("arial",14),background="yellow")
-btn_open.grid(row=5, column=1)
+btn_dot = tk.Button(root, text=".", command=lambda: add_to_calculation("."), width=5, font= ("arial",14),background="yellow")
+btn_dot.grid(row=5, column=3)
 
-btn_close = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font= ("arial",14),background="yellow")
-btn_close.grid(row=5, column=3)
+btn_zero = tk.Button(root, text="00", command=lambda: add_to_calculation("00"), width=5, font= ("arial",14),background="yellow")
+btn_zero.grid(row=5, column=1)
 
-btn_clear = tk.Button(root, text="C", command=clear_filed , width=11, font= ("arial",14),background="yellow")
-btn_clear.grid(row=6, column=1, columnspan=2)
+btn_percent = tk.Button(root, text="%", command=lambda: add_to_calculation("%"), width=5, font= ("arial",14),background="yellow")
+btn_percent.grid(row=6, column=1)
 
-btn_eual = tk.Button(root, text="=", command=evaluate_calculation, width=11, font= ("arial",14),background="yellow")
-btn_eual.grid(row=6, column=3, columnspan=2)
+btn_delete = tk.Button(root, text="DEL", command=delete_filed, width=11, font= ("arial",14),background="orange")
+btn_delete.grid(row=6, column=3, columnspan=2)
+
+btn_clear = tk.Button(root, text="C", command=clear_filed , width=11, font= ("arial",14),background="orange")
+btn_clear.grid(row=7, column=1, columnspan=2)
+
+btn_eual = tk.Button(root, text="=", command=evaluate_calculation, width=11, font= ("arial",14),background="orange")
+btn_eual.grid(row=7, column=3, columnspan=2)
 
 root.mainloop()
